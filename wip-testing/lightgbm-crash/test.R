23270d7ac4c6179c9e505ae5d457a835feb43e3a
szilard <szilard@users.noreply.github.com>
2019-04-30 10:27:45 -0700

    GPU lightgbm crash

---
+++ wip-testing/lightgbm-crash/test.R
@@ -0,0 +1,27 @@
+library(lightgbm)
+data(agaricus.train, package = "lightgbm")
+train <- agaricus.train
+train$data[, 1] <- 1:6513
+dtrain <- lgb.Dataset(train$data, label = train$label)
+data(agaricus.test, package = "lightgbm")
+test <- agaricus.test
+dtest <- lgb.Dataset.create.valid(dtrain, test$data, label = test$label)
+valids <- list(test = dtest)
+
+params <- list(objective = "regression",
+               metric = "rmse",
+               device = "gpu",
+               gpu_platform_id = 0,
+               gpu_device_id = 0,
+               nthread = 1,
+               boost_from_average = FALSE,
+               num_tree_per_iteration = 10,
+               max_bin = 32)
+model <- lgb.train(params,
+                   dtrain,
+                   2,
+                   valids,
+                   min_data = 1,
+                   learning_rate = 1,
+                   early_stopping_rounds = 10)
+                   
