d05654d942a741b5b5f723bcccfa07bb03bf146e
szilard <szilard@users.noreply.github.com>
2019-05-12 00:04:47 -0700

    xgb old laptop

--- wip-testing/xgboost-old_laptop/run/2-xgboost.R
+++ wip-testing/xgboost-old_laptop/run/2-xgboost.R
@@ -19,6 +19,7 @@ X_test <- X_train_test[(n1+1):(n1+n2),]
 
 dxgb_train <- xgb.DMatrix(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0))
 
+cat("Starting training...")
 
 cat(system.time({
   md <- xgb.train(data = dxgb_train, 
