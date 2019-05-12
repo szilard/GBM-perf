7442d159d49a3327b09556d3d252725efd6acad0
szilard <szilard@users.noreply.github.com>
2019-05-12 09:35:50 -0700

    lgbm GPU utilization

--- wip-testing/GPU_utilization_patterns/lightgbm.R
+++ wip-testing/GPU_utilization_patterns/lightgbm.R
@@ -17,7 +17,8 @@ n2 <- nrow(d_test)
 X_train <- X_train_test[1:n1,]
 X_test <- X_train_test[(n1+1):(n1+n2),]
 
-dlgb_train <- lgb.Dataset(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0))
+##dlgb_train <- lgb.Dataset(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0))
+dlgb_train <- lgb.Dataset(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0), nthread = 8)
 #system.time({
 #  lgb.Dataset.construct(dlgb_train)
 #})
