a69151c36ebdb00c1d6f2b4afa9fb5338a208f95
szilard <szilard@users.noreply.github.com>
2019-05-18 04:34:17 -0700

    multicore

--- wip-testing/multicore/run/run.sh
+++ wip-testing/multicore/run/run.sh
@@ -5,11 +5,11 @@ for SIZE in 0.1 1 10; do
   for CORES in $(cat cores.conf); do
     NCORES=$(echo $CORES | cut -d: -f1)
     LCORES=$(echo $CORES | cut -d: -f2)
-    MCORES=$(echo $CORES | cut -d: -f3)
+    TCORES=$(echo $CORES | cut -d: -f3)
     for TOOL in h2o xgboost lightgbm; do
       for i in {1..3}; do
         RUNTIME=$(taskset -c $LCORES R --slave < $TOOL.R $NCORES | tail -1)
-        echo $SIZE:$TOOL:$NCORES:$LCORES:$MCORES:$RUNTIME
+        echo $SIZE:$TOOL:$NCORES:$LCORES:$TCORES:$RUNTIME
       done
     done
   done
