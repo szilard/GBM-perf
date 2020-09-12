3c6adc9d7345fb2ebe328fce16ce6a3b19ce41c4
szilard <szilard@users.noreply.github.com>
2020-09-12 12:09:15 -0700

    add lightgbm cats (vs OHE) and catboost to multicore

--- analysis/multicore/run/run.sh
+++ analysis/multicore/run/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-for SIZE in 0.1 1 10; do
-##for SIZE in 1; do
+##for SIZE in 0.1 1 10; do
+for SIZE in 0.1; do
   ln -sf /train-${SIZE}m.csv train.csv 
   for CORES in $(cat cores.conf); do
     NCORES=$(echo $CORES | cut -d: -f1)
     LCORES=$(echo $CORES | cut -d: -f2)
     TCORES=$(echo $CORES | cut -d: -f3)
     ##for TOOL in h2o xgboost lightgbm; do
-    for TOOL in xgboost lightgbm; do
+    for TOOL in xgboost lightgbm catboost; do
       for i in {1..3}; do
         RUNTIME=$(taskset -c $LCORES R --slave < $TOOL.R $NCORES | tail -1)
         echo $SIZE:$TOOL:$NCORES:$LCORES:$TCORES:$RUNTIME
