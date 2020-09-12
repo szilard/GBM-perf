6e4ca3bf4476a3f68ae8c337597f52648b14c88c
szilard <szilard@users.noreply.github.com>
2020-09-12 12:02:40 -0700

    switch lightgbm from OHE tp cats in the main benchmark

--- cpu/run/3-lightgbm.R
+++ cpu/run/3-lightgbm.R
@@ -10,25 +10,29 @@ set.seed(123)
 d_train <- fread("train.csv", showProgress=FALSE)
 d_test <- fread("test.csv", showProgress=FALSE)
 
+d_all <- rbind(d_train, d_test)
+d_all$dep_delayed_15min <- ifelse(d_all$dep_delayed_15min=="Y",1,0)
 
-X_train_test <- sparse.model.matrix(dep_delayed_15min ~ .-1, data = rbind(d_train, d_test))
-n1 <- nrow(d_train)
-n2 <- nrow(d_test)
-X_train <- X_train_test[1:n1,]
-X_test <- X_train_test[(n1+1):(n1+n2),]
+d_all_wrules <- lgb.convert_with_rules(d_all)       
+d_all <- d_all_wrules$data
+cols_cats <- names(d_all_wrules$rules) 
 
-dlgb_train <- lgb.Dataset(data = X_train, label = ifelse(d_train$dep_delayed_15min=='Y',1,0))
+d_train <- d_all[1:nrow(d_train)]
+d_test <- d_all[(nrow(d_train)+1):(nrow(d_train)+nrow(d_test))]
 
+p <- ncol(d_all)-1
+dlgb_train <- lgb.Dataset(data = as.matrix(d_train[,1:p]), label = d_train$dep_delayed_15min)
 
 cat(system.time({
   md <- lgb.train(data = dlgb_train, 
             objective = "binary", 
             nrounds = 100, num_leaves = 512, learning_rate = 0.1, 
+            categorical_feature = cols_cats,
             verbose = 0)
 })[[3]]," ",sep="")
 
 
-phat <- predict(md, data = X_test)
+phat <- predict(md, data = as.matrix(d_test[,1:p]))
 rocr_pred <- prediction(phat, d_test$dep_delayed_15min)
 cat(performance(rocr_pred, "auc")@y.values[[1]],"\n")
 
