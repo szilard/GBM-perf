ee4d8d47e3af42bc16138d2c80b2674029672bda
szilard <szilard@users.noreply.github.com>
2020-09-19 01:30:06 -0700

    h2o super-linear

--- wip-testing/h2o-super_linear/run/run.sh
+++ wip-testing/h2o-super_linear/run/run.sh
@@ -6,7 +6,7 @@ for SIZE in 0.1 10; do
     NCORES=$(echo $CORES | cut -d: -f1)
     LCORES=$(echo $CORES | cut -d: -f2)
     TCORES=$(echo $CORES | cut -d: -f3)
-    for TOOL in h2o xgboost lightgbm catboost; do
+    for TOOL in h2o; do
       for i in {1..2}; do
         RUNTIME=$(taskset -c $LCORES R --slave < $TOOL.R $NCORES | tail -1)
         echo $SIZE:$TOOL:$NCORES:$LCORES:$TCORES:$RUNTIME
