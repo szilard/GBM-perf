c3ac7b753d5fd08d7fa96ab74c4af9fb82f56e48
szilard <szilard@users.noreply.github.com>
2020-09-19 06:22:51 -0700

    h2o super-linear

--- wip-testing/h2o-super_linear/run/h2o.R
+++ wip-testing/h2o-super_linear/run/h2o.R
@@ -17,7 +17,8 @@ Xnames <- names(dx_train)[which(names(dx_train)!="dep_delayed_15min")]
 cat(system.time({
   md <- h2o.gbm(x = Xnames, y = "dep_delayed_15min", training_frame = dx_train, 
           distribution = "bernoulli", 
-          ntrees = 100, max_depth = 10, learn_rate = 0.1, 
+          ##ntrees = 100, max_depth = 10, learn_rate = 0.1, 
+          ntrees = 10, max_depth = 10, learn_rate = 0.1, 
           nbins = 100)
 })[[3]],":",sep="")
 
