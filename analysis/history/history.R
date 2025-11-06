019f56c2e258e627e8090efa315cee46a75756cb
szilard <szilard@users.noreply.github.com>
2025-11-06 08:53:24 +0100

    history updated to include 2025

---
+++ analysis/history/history.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+library(data.table)
+
+d <- fread("history.csv")
+d
+dm <- melt(d, id.vars = c("platform","year","size"))
+dm
+
+colnames(dm) <- c("platform","year","size","software","runtime")
+
+ggplot(dm, aes(x = year, y = runtime, color = software)) +
+  geom_line(data = subset(dm, year <= 2024)) +
+  geom_line(data = subset(dm, year >= 2024), linetype = "dashed") +
+  geom_point() +
+  scale_y_log10() +
+  facet_grid(size ~ platform) +
+  theme_minimal()
